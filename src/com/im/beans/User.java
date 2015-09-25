561a88353a839d72d8732f5704549986b80700fc
sreeshan <sreeshan@RZTL607-PC.rzt.com>
2015-09-25 18:47:14 +0530

    java files added !

--- src/com/im/beans/User.java
+++ src/com/im/beans/User.java
@@ -1,9 +1,29 @@
 package com.im.beans;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class User {
 
+	@Id
+	@GeneratedValue( strategy = GenerationType.IDENTITY )
 	private int userId;
 	private String userName;
+	private String emaild;
+	private long phoneNumber;
+
+	public User()
+	{
+
+	}
+
+	public User( String name )
+	{
+		userName = name;
+	}
 
 	public int getUserId()
 	{
@@ -23,6 +43,26 @@ public class User {
 	public void setUserName( String userName )
 	{
 		this.userName = userName;
+	}
+
+	public String getEmaild()
+	{
+		return emaild;
+	}
+
+	public void setEmaild( String emaild )
+	{
+		this.emaild = emaild;
+	}
+
+	public long getpNumber()
+	{
+		return phoneNumber;
+	}
+
+	public void setpNumber( long pNumber )
+	{
+		this.phoneNumber = pNumber;
 	}
 
 }
