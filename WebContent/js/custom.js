f65e9afcab81b6b4afc2218074118413e63888d5
kingspp <kingspprathyush@gmail.com>
2015-09-25 13:44:12 +0530

    Added url-endpoints

--- WebContent/js/custom.js
+++ WebContent/js/custom.js
@@ -1,5 +1,6 @@
 //Variable Declaration
 var version = "Beta v0.0.1";
+var urlPath = window.location.href+"welcome/hello/query=";
 
 $(".version").html(version); 
 
@@ -9,7 +10,7 @@ $(".user").click( function(){
 	$("#dispAlpha").hide();
 	var id = 97;
 	while(id < (97 + 26)){			
-		$("#dispAlpha").append("<a href=\"http://www.google.com\">"+String.fromCharCode(id-32)+"</a>&nbsp&nbsp");		
+		$("#dispAlpha").append("<a href=\""+urlPath+String.fromCharCode(id-32)+"\">"+String.fromCharCode(id-32)+"</a>&nbsp&nbsp");		
 		id++;
 	}
 	$("#dispAlpha").slideDown( "slow", function() {});
